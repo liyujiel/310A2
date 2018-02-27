7b2dc0d1288b465f9d26a2729648ea0adee0a58c
Alex Li <li1995717@gmail.com>
2018-02-26 22:35:33 -0800

    add asst2.py

--- a2.py
+++ a2.py
@@ -1,45 +1,127 @@
-G = [[1,2,3,4,6,7,10],[2,1,3,4,5,6],[3,1,2],[4,1,2],[5,2,6],[6,1,2,5,7,8]]#[[1,2,3], [2,1,3], [3,1,2], [4,5], [5,4]] # a list of lists
-n = 6 # number of vertices
-k = 4 # number of colours
+# For the assignment, implement basic backtracking search along with the MRV, degree,
+# and least constraining value heuristic. In your documentation, make sure that you describe
+# your program at a high level and discuss any interesting aspects of your program. Test
+# data is in a separate file, asking for a 4-colouring of 10 countries. Run your program on
+# this data, as well as trying for a 3-colouring (which will fail). As well, optionally, test your
+# program with and without using the heuristics, and report on the improvement (or lack of
+# improvement) obtained.
 
 
-# (6 1 2 5 7 8)
-# (7 1 6 8 9 10)
-# (8 6 7 9)
-# (9 7 8 10)
-# (10 1 7 9)
+# Function Backtracking-Search(csp) returns solution/failure
+# return Recursive-Backtracking({ }, csp)
+# Function Recursive-Backtracking(assignment, csp) returns soln/failure
+#   if assignment is complete then return assignment
+#   var ←Select-Unassigned-Variable(Variables[csp], assignment, csp)
+#   for each value in Order-Domain-Values(var, assignment, csp) do
+#       if value is consistent with assignment given Constraints[csp] then
+#          add {var = value} to assignment
+#          result ←Recursive-Backtracking(assignment, csp)
+#          if result 6= failure then
+#            return result
+#          remove {var = value} from assignment
+#   return failure
 
-class Node:
-    def __init__(self,val,neighbours,color):
-        self.val = val
-        self.neighbours = []
-        self.color = color
-        for neighbour in neighbours:
-            self.neighbours.append(neighbour)
+from queue import PriorityQueue
+
+def isComplete(assignment,n):
+    if(len(assignment) == n):
+        return True
+    return False
+
+def pickVertex(unassign):
+    color_lens = []
+    for index in unassign:
+        color_lens.append(len(color[index-1]))
+    opt_size = min(color_lens)
+    min_poses = [pos for pos,val in enumerate(color_lens) if val == opt_size]
+    return  unassign[min_poses[0]]+1
+
+def heuristic(vertex, pos):
+    res = 0
+    for node in unassign:
+        # current node is veterx's neighbour
+        if node != vertex and node in G[vertex-1]:
+            res  =  res + len(color[vertex-1])
+            if pos in color[node-1]:
+                res -= 1
+    return res
+
+
+def createColorPq(vertex):
+    pq = PriorityQueue()
+    for i in range(1,k+1):
+        pq.put((heuristic(vertex,i),i))
+    return pq
+
+def isConsistant(vertex, curr_color):
+    for neighbour in G[vertex-1]:
+        if neighbour != G[vertex-1][0]:
+            for assignment in assignments:
+                if assignment[0] == neighbour and curr_color == assignment[0]:
+                    return False
+    return True
+
+def removeColor(curr_vertex, curr_color):
+    for vertex in G[curr_vertex]:
+        if (vertex-1) in set(unassign):
+            color[vertex-1].remove(curr_color)
+
+def solve(n, k, G):
+    if(isComplete(assignments,n)):
+        return assignments
+
+    curr_vertex = pickVertex(unassign)
+    # print(curr_vertex)
+    colorPq = createColorPq(curr_vertex)
+
+    for _ in range(k):
+        curr_color = colorPq.get()[1]
+        # print(curr_color)
+
+        if isConsistant(curr_vertex, curr_color):
+            # print(curr_vertex)
+            assignments.append((curr_vertex,curr_color))
+            assigned.append(curr_vertex)
+            unassign.remove(curr_vertex)
+            # for vertex in G[curr_vertex-1]:
+            #     if(vertex in unassign):
+            #         # print(color)
+            #         color[vertex-1].remove(curr_color)
+            removeColor(curr_vertex,curr_color)
+
             
+            ans = solve(n,k,G)
 
-topo_graph = {}
+            if ans != []:
+                return ans
+            
+            assignments.remove((curr_vertex,curr_color))
+            assigned.remove(curr_vertex)
+            for vertex in G[curr_vertex]:
+                 if(vertex in unassign):
+                    color[vertex].append(curr_color)
+            unassign.append(curr_vertex)
+    
+    return []
 
-for vetric in G:
-    # create new node and set it color as first color
-    new_node = Node(vetric[0],vetric[1::],1)
-    if not topo_graph.has_key(new_node.val):
-        topo_graph[new_node.val] = new_node.color
-        for neighbour in new_node.neighbours:
-            if(not topo_graph.has_key(neighbour)):
-                topo_graph[neighbour] = new_node.color+1
-            else:
-                if(topo_graph.get(neighbour) > new_node.color+1):
-                    topo_graph[neighbour] = new_node.color+1
-    else:
-        for neighbour in new_node.neighbours:
-            if(new_node.val < neighbour):
-                topo_graph[neighbour] = topo_graph.get(new_node.val)+1
 
-for key in topo_graph:
-    print(key, " ", topo_graph[key])
+# Input 
+G = [[1,2,3], [2,1,3], [3,1,2], [4,5], [5,4]] # a list of lists
+n = 5  # number of vertices
+k = 3  # number of colours
 
-# def solve(n, k, G):
-#     # write your code here
-#     # return solution
-#     pass
+# G = [ [1,2 ,3, 4, 6, 7, 10],[2, 1, 3, 4, 5, 6],[3, 1, 2],[4, 1, 2],[5, 2, 6],[6, 1, 2, 5, 7, 8],[7, 1, 6, 8, 9 ,10],[8, 6, 7, 9],[9, 7, 8, 10],[10, 1, 7, 9]]
+# n = len(G)
+# k = 4
+
+# inital result assigned nodes, unasigned nodes and assignments for check compelete or not
+ans = []
+assignments = []
+unassign = list(range(1,n+1))
+assigned = []
+color = []
+for i in range(n):
+    color.append(list(range(1,k + 1)))
+
+# solve(n,k,G)
+print(solve(n,k,G))
---
+++ asst2.py
@@ -0,0 +1,136 @@
+# For the assignment, implement basic backtracking search along with the MRV, degree,
+# and least constraining value heuristic. In your documentation, make sure that you describe
+# your program at a high level and discuss any interesting aspects of your program. Test
+# data is in a separate file, asking for a 4-colouring of 10 countries. Run your program on
+# this data, as well as trying for a 3-colouring (which will fail). As well, optionally, test your
+# program with and without using the heuristics, and report on the improvement (or lack of
+# improvement) obtained.
+
+
+# Function Backtracking-Search(csp) returns solution/failure
+# return Recursive-Backtracking({ }, csp)
+# Function Recursive-Backtracking(assignment, csp) returns soln/failure
+#   if assignment is complete then return assignment
+#   var ←Select-Unassigned-Variable(Variables[csp], assignment, csp)
+#   for each value in Order-Domain-Values(var, assignment, csp) do
+#       if value is consistent with assignment given Constraints[csp] then
+#          add {var = value} to assignment
+#          result ←Recursive-Backtracking(assignment, csp)
+#          if result 6= failure then
+#            return result
+#          remove {var = value} from assignment
+#   return failure
+
+from queue import PriorityQueue
+
+def isComplete(assignment,n):
+    if len(assignment) == n:
+        return True
+    return False
+
+def pickVeterx(unassigned):
+    # get the indexes for unassigned nodes
+    indexes = [(x - 1) for x in unassigned]
+    uncolor = [color[x] for x in indexes]
+    colorlen = [len(x) for x in uncolor]
+    # check the duplication of min 
+    m = min(colorlen)
+    dup = [i for i, j in enumerate(colorlen) if j == m]
+    return indexes[dup[0]] +1
+
+
+    # color_lens = []
+    # for index in unassigned:
+    #     color_lens.append(len(color[index-1]))
+    # opt_size = min(color_lens)
+    # min_poses = [pos for pos,val in enumerate(color_lens) if val == opt_size]
+    # return  unassigned[min_poses[0]]+1
+
+def checkConsistant(curr_vertex, curr_color):
+    for neighbour in G[curr_vertex - 1]:
+        if neighbour != G[curr_vertex - 1][0]:
+            # If any adjacet nodes has same colour, return false instantly
+            for node in assignment:
+                if neighbour == node[0] and curr_color == node[1]:
+                    return False
+    return True
+                
+  
+def heuristic(curr_vertex, temp_color):
+    count = 0
+    for node in unassigned:
+        if node != curr_vertex and node in G[curr_vertex - 1] :
+            count += len(color[node - 1])
+            if temp_color in color[node-1]:
+                count -= 1
+    return count 
+ 
+    
+def pickColor(curr_vertex):
+    pq = PriorityQueue()
+
+    for temp_color in range(1,k + 1):
+        pq.put((heuristic(curr_vertex, temp_color),temp_color))
+    
+    return pq
+
+def removeColor(curr_vertex,curr_color):
+    for neighbour in G[curr_vertex - 1]:
+        if neighbour in unassigned:
+            color[neighbour - 1].remove(curr_color)
+
+def addColor(curr_vertex, curr_color):
+    for neighbour in G[curr_vertex - 1]:
+        if neighbour in unassigned:
+            color[neighbour - 1].append(curr_color) 
+      
+
+def solve(n, k, G):
+    # write your code here
+    if isComplete(assignment,n):
+        return assignment
+
+    curr_vertex = pickVeterx(unassigned)
+    colorPriority = pickColor(curr_vertex)
+    # print(curr_vertex)
+    for _ in range(k):
+        i = colorPriority.get()[1]
+        
+        if checkConsistant(curr_vertex,i):
+            assignment.append((curr_vertex,i))
+            assigned.append(curr_vertex)
+            unassigned.remove(curr_vertex)
+            removeColor(curr_vertex,i)
+          
+            ans = solve(n,k,G)
+            if ans != []:
+                return ans
+
+            assignment.remove((curr_vertex,i))
+            assigned.remove(curr_vertex)
+            addColor(curr_vertex,i)
+            unassigned.append(curr_vertex)
+
+    return []
+
+    
+# Input 
+G = [[1,2,3], [2,1,3], [3,1,2], [4,5], [5,4]] # a list of lists
+n = 5  # number of vertices
+k = 3  # number of colours
+
+G = [[1,2 ,3, 4, 6, 7, 10],[2, 1, 3, 4, 5, 6],[3, 1, 2],[4, 1, 2],[5, 2, 6],[6, 1, 2, 5, 7, 8],[7, 1, 6, 8, 9 ,10],[8, 6, 7, 9],[9, 7, 8, 10],[10, 1, 7, 9]]
+n = len(G)
+k = 4
+
+# inital result assigned nodes, unasigned nodes and assignments for check compelete or not
+ans = []
+assignment = []
+unassigned = list(range(1,n+1))
+assigned = []
+color = []
+for i in range(n):
+    color.append(list(range(1,k + 1)))
+
+# solve(n,k,G)
+print(solve(n,k,G))
